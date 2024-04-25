b5a4b1f3edbb45a505d8dc133120b8cad8934157
Alan Zanette Stefenon <98592258+zanette90@users.noreply.github.com>
2024-04-25 16:36:16 -0300

    Initial commit

---
+++ src/ConsultaCep.java
@@ -0,0 +1,23 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaCep {
+    public Endereco buscaEndereco(String cep) {
+        try {
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco)
+                .build();
+
+            HttpResponse<String> response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (Exception f) {
+            throw new RuntimeException("Não foi possível o endereço a partir desse CEP.");
+        }
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+    public class Principal {
+        public static void main(String[] args) {
+            while (true) {
+                Scanner leitura = new Scanner(System.in);
+                ConsultaCep consultaCep = new ConsultaCep();
+
+                System.out.println("Digite um número de CEP para consulta:");
+                var cep = leitura.nextLine();
+
+                try {
+                    Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+                    System.out.println(novoEndereco);
+                    GeradorDeArquivo gerador = new GeradorDeArquivo();
+                    gerador.salvaJson(novoEndereco);
+                } catch (RuntimeException | IOException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("Finalizando a aplicação");
+                }
+            }
+        }
+    }
+
